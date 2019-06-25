f2c7ca912ce49b2a7b2d20c7affaf07905e3706b
Emma Borhanian <emma.borhanian@gmail.com>
2019-06-25 04:05:57 -0400

    download pages with request

--- arbital-scrape.js
+++ arbital-scrape.js
@@ -1,4 +1,7 @@
 #!/usr/bin/env node
+let fs = require('fs-extra')
+let request = require('request-promise-native')
+let sanitizeFilename = require('sanitize-filename')
 
 let argv = require('yargs')
   .command('$0 [directory]', 'scrape arbital.com', yargs=>
@@ -8,8 +11,41 @@ let argv = require('yargs')
            default: 'arbital.com' })
          .option('url', {
            desc: 'arbital url',
-           default: 'https://arbital.com' }))
+           default: 'https://arbital.com' })
+         .option('page', {
+           desc: 'pages to download or start at',
+           array: true,
+           string: true,
+           default: ['arbital_front_page'] }))
   .help()
   .argv
 
-console.log('argv', argv)
+let requestRawPage = async aliasOrId=>{
+  let url = `${argv.url}/json/primaryPage/`
+  let body = { pageAlias: aliasOrId }
+  console.log('fetching', aliasOrId, 'POST', url, body)
+  return await request({
+    method: 'POST',
+    url: url,
+    json: true,
+    body: body })
+}
+
+let findPageInRawPageJson = (aliasOrId, rawPageJson)=> rawPageJson.pages[aliasOrId] || Object.values(rawPageJson.pages).find(p=>p.alias==aliasOrId)
+
+;(async ()=>{
+  await fs.mkdirp(argv.directory)
+
+  let toDownload = Array.from(argv.page)
+  while (toDownload.length > 0) {
+    let aliasOrId = toDownload.pop()
+    let rawPage = await requestRawPage(aliasOrId)
+    let page = findPageInRawPageJson(aliasOrId, rawPage)
+    aliasOrId = page.alias || page.pageId
+
+    let file = `${argv.directory}/raw/${sanitizeFilename(page.pageId)}.json`
+    console.log('writing', aliasOrId, file)
+    await fs.mkdirp(`${argv.directory}/raw`)
+    await fs.writeJson(file, rawPage, {spaces: 2})
+  }
+})()
